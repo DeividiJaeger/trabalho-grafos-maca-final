0f3c4c49127c0b66a1d4d87198ef8d8526caa998
DeividiJaeger <deividi.contato@gmail.com>
2024-06-27 12:00:49 -0300

    Criação struct da Aresta, No e Grafo

--- grafo.c
+++ grafo.c
@@ -1,2 +1,29 @@
-#include "grafo.h"
+// #include "grafo.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Definindo a estrutura de uma aresta
+typedef struct Aresta 
+{
+    int destino;
+    void* dado;
+    struct Aresta* prox;
+} Aresta;
+
+// Definindo Estrutura de um dado (No)
+typedef struct No 
+{
+    void* dado;
+    Aresta* listaArestas;
+} No;
+
+// Definindo a Estrutura do próprio Grafo
+typedef struct Grafo 
+{
+    int numVertices;
+    int tamNo;
+    int tamArestas;
+    No* vertices;
+} Grafo;
 
